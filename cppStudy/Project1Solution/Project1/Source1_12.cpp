fc4cc64368d5aa33fcb292be3ebf20de9f575ecd
김기덕 <ikid4127@naver.com>
2021-09-23 21:31:36 +0900

    헤더 가드와 링킹 에러 학습

---
+++ cppStudy/Project1Solution/Project1/Source1_12.cpp
@@ -0,0 +1,14 @@
+// 헤더 가드 Header Guards
+
+#include <iostream>
+#include "dosomething.h"
+#include "add.h"
+
+using namespace std;
+
+int main()
+{
+	doSomething();
+
+	return 0;
+}
--- cppStudy/Project1Solution/Project1/add.h
+++ cppStudy/Project1Solution/Project1/add.h
@@ -1,3 +1,12 @@
-#pragma once
+//#pragma once
 
-int add(int, int);
+#ifndef  MY_ADD // 만약 MY_ADD가 정의되지 않았다면
+#define  MY_ADD	// MY ADD를 정의하고
+
+// 이걸 정의
+int add(int a, int b)
+{
+	return a + b;
+}
+
+#endif // ! MY_ADD
