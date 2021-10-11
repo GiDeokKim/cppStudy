3e61d2d3a3a6d2e414433020391af6176a2c4d32
김기덕 <ikid4127@naver.com>
2021-10-11 14:21:20 +0900

    Operator Precedence and Associativity

---
+++ cppStudy/Project1Solution/Project3/Source3_1.cpp
@@ -0,0 +1,16 @@
+// 연산자 우선순위와 결합법칙
+// Operator Precedence and Associativity
+// 관련 내용은 검색으로 확인하는게 좋다.
+// 순서가 애매하다면 프로그래머가 직접 ()를 통해 순서를 확실히 한다.
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	// 약속된 연산자 우선순위로 계산된다.
+	int x = 4 + 2 * 3;
+
+	return 0;
+}
