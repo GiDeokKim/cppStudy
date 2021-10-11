8ba5c0d6694b4662e0e3e71cc52504ac34bccc7c
김기덕 <ikid4127@naver.com>
2021-10-11 15:54:17 +0900

    부동소수점수 복습 및 코드 정리

--- cppStudy/Project1Solution/Project2/Source2_5.cpp
+++ cppStudy/Project1Solution/Project2/Source2_5.cpp
@@ -15,34 +15,29 @@ int main()
 {
 	using namespace std;
 
-	float f(3.141592f);
-	double d(3.141592);
-	long double ld(3.141592);
+	cout << sizeof(float)		<< endl; // 4 bytes
+	cout << sizeof(double)		<< endl; // 8 bytes
+	cout << sizeof(long double) << endl; // 8 bytes
 
-	cout << sizeof(float) << endl;
-	cout << sizeof(d) << endl;
-	cout << sizeof(ld) << endl;
-
-	cout << numeric_limits<float>::max() << endl; // min() lowest()
-	cout << numeric_limits<double>::max() << endl;
-	cout << numeric_limits<long double>::max() << endl;
+	cout << numeric_limits<float>::max()		<< endl; // min() lowest()
+	cout << numeric_limits<double>::max()		<< endl;
+	cout << numeric_limits<long double>::max()	<< endl;
 
 	// scientific notation
-	cout << 3.14 << endl;
-	cout << 31.4e-1 << endl;
-	cout << 31.4e-2 << endl;
-	cout << 31.4e1 << endl;
-	cout << 31.4e2 << endl;
+	cout << 3.14	<< endl;
+	cout << 31.4e-1	<< endl;
+	cout << 31.4e-2	<< endl;
+	cout << 31.4e1	<< endl;
+	cout << 31.4e2	<< endl;
 
-	//cout << std::setprecision(16);
-	cout << 1.0 / 3.0 << endl;
+	cout << std::setprecision(25);
+	cout << 1.0f / 3.0f	<< endl;
+	cout << 1.0 / 3.0	<< endl;
 
 	float f1 = 123456789.0f; // 10 significant digits
 	cout << f1 << endl;
 
 	double d1 = 1.0;
-	cout << d1 << endl;
-	cout << std::setprecision(17);
 	cout << d1 << endl;
 	cout << std::setprecision(30);
 	cout << d1 << endl;
@@ -50,18 +45,18 @@ int main()
 	double d2 = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
 	cout << d2 << endl; // 콘솔창에서 표기된 수는 setprecision으로 정확히 보는게 아닌 이상 믿을 수 없다.
 
-	double zero = 0.0;
-	double posinf = 5.0 / zero;
-	double neginf = -5.0 / zero;
-	double nan = zero / zero;
+	double zero		= 0.0;
+	double posinf	= 5.0 / zero;
+	double neginf	= -5.0 / zero;
+	double nan		= zero / zero;
 
-	cout << posinf << endl;
-	cout << neginf << endl;
-	cout << nan << endl; // -nan(ind) indeterminate
-	cout << posinf << " " << std::isinf(posinf) << endl;
-	cout << neginf << " " << std::isinf(neginf) << endl;
-	cout << nan << " " << std::isnan(nan) << endl;
-	cout << 1.0 << " " << std::isnan(1.0) << endl;
+	cout << posinf	<< endl;
+	cout << neginf	<< endl;
+	cout << nan		<< endl; // -nan(ind) indeterminate
+	cout << posinf	<< " " << std::isinf(posinf) << endl;
+	cout << neginf	<< " " << std::isinf(neginf) << endl;
+	cout << nan		<< " " << std::isnan(nan) << endl;
+	cout << 1.0		<< " " << std::isnan(1.0) << endl;
 
 	return 0;
 }
