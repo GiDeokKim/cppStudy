d1f09847ef42a63ec48fdf39924ef03142f99479
김기덕 <ikid4127@naver.com>
2021-10-17 14:49:06 +0900

    literal constants 복습 및 코드 정리

--- cppStudy/Project1Solution/Project2/Source2_8.cpp
+++ cppStudy/Project1Solution/Project2/Source2_8.cpp
@@ -1,6 +1,10 @@
 // 리터럴 상수 Literal Constants
 // c++ 14 Binary Literals도 추가
 
+// Decimal	: 0 1 2 3 4 5 6 7 8 9 10
+// Octal	: 0 1 2 3 4 5 6 7 10
+// Hexa		: 0 1 2 3 4 5 6 7 8 9 A B C D E F 10
+
 #include <iostream>
 
 int main()
@@ -13,10 +17,6 @@ int main()
 	unsigned int n = 5u;
 	long n2 = 5L;
 
-	// Decimal	: 0 1 2 3 4 5 6 7 8 9 10
-	// Octal	: 0 1 2 3 4 5 6 7 10
-	// Hexa		: 0 1 2 3 4 5 6 7 8 9 A B C D E F 10
-
 	int x = 0xF;
 	cout << x << endl;
 	x = 012;
@@ -25,9 +25,6 @@ int main()
 	cout << x << endl;
 	x = 0b1011'1111'1010; // 컴파일러는 '를 무시한다. 10진수에도 사용가능하다.
 	cout << x << endl;
-
-	int num_items = 123;
-	int price = num_items * 10; // 10 is .... 하드코딩으로 수를 남기는 것보다는 심볼릭 상수를 쓰는게 더 좋다
 
 	return 0;
 }
