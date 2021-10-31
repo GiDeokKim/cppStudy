2370884581739f6cb2224b49b33ddda82d5a9a79
김기덕 <ikid4127@naver.com>
2021-10-31 15:20:10 +0900

    산술 연산자 복습

--- cppStudy/Project1Solution/Project3/Source3_2.cpp
+++ cppStudy/Project1Solution/Project3/Source3_2.cpp
@@ -10,24 +10,28 @@ int main()
 	int x = 1;
 
 	// 단항 연산자 +, -
-	int y = -x;
-	int z = +x;
+	int unaryOperatorMinus	= -x;
+	int unaryOperatorPlus	= +x;
 
 	// 이항 연산자 +, -, *, /, %
-	x = 7;
-	y = 4;
-	cout << x / y				<< endl;
-	cout << float(x) / y		<< endl;
-	cout << x / float(y)		<< endl;
-	cout << float(x) / float(y) << endl;
-	
-	cout << -5 / 2 << endl; // c++ 11부터는 소수점 아래를 절삭하기로 했음
-	cout << -5 % 2 << endl;
-	cout << -5 % -2 << endl;
-	cout << 5 % -2 << endl;
+	int operand1 = 7;
+	int operand2 = 4;
 
+	// c++ 11부터는 소수점 아래를 절삭하기로 했음
+	cout << operand1 / operand2					<< endl; // return  1
+	cout << float(operand1) / operand2			<< endl; // return  1.75
+	cout << operand1 / float(operand2)			<< endl; // return  1.75
+	cout << float(operand1) / float(operand2)	<< endl; // return  1.75
+	
+	cout << -5	/  2	<< endl; // return -2
+	cout << -5	%  2	<< endl; // return -1
+	cout << -5	% -2	<< endl; // return -1
+	cout <<  5	% -2	<< endl; // return  1
+
+	// 복합 연산자
 	// 코딩 작업 내용을 줄여준다.
 	// 실수를 줄여준다. 보기가 편해진다.
-	z += y;
+	operand2 += operand1;
+
 	return 0;
 }
