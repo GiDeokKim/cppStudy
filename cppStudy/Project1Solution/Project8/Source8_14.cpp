91c50160c3d44a1fbd39a35bb24b75ffdf7f11da
김기덕 <ikid4127@naver.com>
2021-11-10 23:29:26 +0900

    [학습] 8.14 Nested types

---
+++ cppStudy/Project1Solution/Project8/Source8_14.cpp
@@ -0,0 +1,29 @@
+// 클래스 안에 포함된 자료형 Nested types
+#include <iostream>
+
+class Fruit
+{
+public:
+	enum FruitType // enum class로도 사용 가능
+	{
+		APPLE, BANANA, CHERRY
+	};
+private:
+	FruitType m_type;
+
+public:
+	Fruit(FruitType type) : m_type(type)
+	{}
+
+	FruitType getType() { return m_type; }
+};
+
+int main()
+{
+	Fruit apple(Fruit::APPLE);
+
+	if (apple.getType == Fruit::APPLE)
+	{
+		std::cout << "Apple" << std::endl;
+	}
+}
