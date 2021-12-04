699ce50676fb150ad426986114fafef4028d6219
김기덕 <ikid4127@naver.com>
2021-12-04 19:15:57 +0900

    [복습] 4.08 영역 제한 열거형 (열거형 클래스)

--- cppStudy/Project1Solution/Project4/Source4_8.cpp
+++ cppStudy/Project1Solution/Project4/Source4_8.cpp
@@ -1,8 +1,37 @@
 // 영역 제한 열거형(열거형 클래스)
 // Scoped Enumerations(Enum Class) Enum Struct도 있긴하다.
+// C++ 11
 
 #include <iostream>
 
+/* 1. enum을 써버리면 말도 안되는 상황이 생겨버림
+int main()
+{
+	using namespace std;
+
+	enum Color
+	{
+		RED,
+		BLUE
+	};
+
+	enum Fruit
+	{
+		BANANA,
+		APPLE
+	};
+
+	Color color = RED;
+	Fruit fruit = BANANA;
+
+	if(color==fruit)
+		cout << "Color is fruit ? " << endl;
+
+	return 0;
+
+}//*/
+
+//* 2. enum class로 실수를 피하자
 int main()
 {
 	using namespace std;
@@ -22,9 +51,9 @@ int main()
 	Color color = Color::RED;
 	Fruit fruit = Fruit::BANANA;
 
-	if (static_cast<int>(color) == static_cast<int>(fruit))
-	//if(color==fruit) // enum class라서 막혀버림
-		cout << "Real?" << endl;
+	if (static_cast<int>(color) == static_cast<int>(fruit)) // 비교가 가능하지만 사용자가 원하는 부분은 아니다.
+	//if(color==fruit) // enum class라서 막혀버림 같은 enum class 안에 들어가는 것들만 비교 가능
+		cout << "Color is fruit ? " << endl;
 
 	return 0;
-}
+}//*/
