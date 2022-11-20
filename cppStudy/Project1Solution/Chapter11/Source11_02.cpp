d4fd3b76a9603aeb35f8aecf4804ffd13e8dc9c0
김기덕 <ikid4127@naver.com>
2022-11-20 20:22:24 +0900

    [학습] 11.2 상속의 기본 2

---
+++ cppStudy/Project1Solution/Chapter11/Source11_02.cpp
@@ -0,0 +1,23 @@
+#include "Student.h"
+#include "Teacher.h"
+
+int main()
+{
+	Student std("Jack Jack");
+	Teacher teacher1("Dr. H");
+
+	std::cout << std << std::endl;
+	std::cout << teacher1 << std::endl;
+
+	std.doNothing();
+	teacher1.doNothing();
+
+	std.study();
+	teacher1.teach();
+
+	Person person;
+	person.setName("Mr. Incredible");
+	person.getName();
+
+	return 0;
+}
