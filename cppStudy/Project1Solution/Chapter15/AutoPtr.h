fe1065aa391cde1d7aa968b77562e64c1c9cdb49
김기덕 <ikid4127@naver.com>
2022-12-02 00:25:55 +0900

    [학습] 15.3 이동 생성자와 이동 대입

--- cppStudy/Project1Solution/Chapter15/AutoPtr.h
+++ cppStudy/Project1Solution/Chapter15/AutoPtr.h
@@ -2,42 +2,141 @@
 
 #include <iostream>
 // std::auto_ptr<int> // C++ 98 ~ 11, 17(deprecated)
+// 15-01
+//template<class T>
+//class AutoPtr
+//{
+//public:
+//	T* m_ptr = nullptr;
+//
+//public:
+//	AutoPtr(T* ptr = nullptr)
+//		: m_ptr(ptr)
+//	{}
+//
+//	~AutoPtr()
+//	{
+//		if (m_ptr != nullptr)
+//		{
+//			delete m_ptr;
+//		}
+//	}
+//
+//	AutoPtr(AutoPtr& a)
+//	{
+//		m_ptr = a.m_ptr;
+//		a.m_ptr = nullptr;
+//	}
+//
+//	AutoPtr& operator=(AutoPtr& a)
+//	{
+//		if (&a == this)
+//		{
+//			return *this;
+//		}
+//
+//		delete m_ptr;
+//		m_ptr = a.m_ptr;
+//		a.m_ptr = nullptr;
+//		return *this;
+//	}
+//
+//	T& operator*() const
+//	{
+//		return *m_ptr;
+//	}
+//
+//	T* operator->() const
+//	{
+//		return m_ptr;
+//	}
+//
+//	bool isNull() const
+//	{
+//		return m_ptr == nullptr;
+//	}
+//};
 
+// 15-03
 template<class T>
 class AutoPtr
 {
 public:
-	T* m_ptr = nullptr;
+	T* m_ptr;
 
 public:
 	AutoPtr(T* ptr = nullptr)
 		: m_ptr(ptr)
-	{}
+	{
+		std::cout << "AutoPtr default constructor " << std::endl;
+	}
 
 	~AutoPtr()
 	{
+		std::cout << "AutoPtr destructor " << std::endl;
+
 		if (m_ptr != nullptr)
 		{
 			delete m_ptr;
 		}
 	}
 
-	AutoPtr(AutoPtr& a)
+	AutoPtr(const AutoPtr& a)
 	{
-		m_ptr = a.m_ptr;
-		a.m_ptr = nullptr;
+		std::cout << "AutoPtr copy constructor " << std::endl;
+
+		// deep copy
+		m_ptr = new T;
+		*m_ptr = *a.m_ptr;
 	}
 
-	AutoPtr& operator=(AutoPtr& a)
+	AutoPtr(const AutoPtr&& a)
+		: m_ptr(a.m_ptr)
 	{
-		if (&a == this)
+		a.m_ptr = nullptr; // really necessary?
+
+		std::cout << "AutoPtr move constructor " << std::endl;
+	}
+
+	AutoPtr& operator=(AutoPtr&& a)
+	{
+		std::cout << "AutoPtr move assignment " << std::endl;
+
+		if (&a == this) // prevent self-assignment
 		{
 			return *this;
 		}
 
-		delete m_ptr;
+		if (m_ptr != nullptr)
+		{
+			delete m_ptr;
+		}
+
+		// shallow copy
 		m_ptr = a.m_ptr;
-		a.m_ptr = nullptr;
+		a.m_ptr = nullptr; // really necessary?
+
+		return *this;
+	}
+
+	AutoPtr& operator=(const AutoPtr& a)
+	{
+		std::cout << "AutoPtr copy assignment " << std::endl;
+
+		if (&a == this) // prevent self-assignment
+		{
+			return *this;
+		}
+
+		if (m_ptr != nullptr)
+		{
+			delete m_ptr;
+		}
+
+		// deep copy
+		m_ptr = new T;
+		*m_ptr = *a.m_ptr;
+
 		return *this;
 	}
 
--- cppStudy/Project1Solution/Chapter15/Resource.h
+++ cppStudy/Project1Solution/Chapter15/Resource.h
@@ -2,19 +2,98 @@
 
 #include <iostream>
 
+// 15-01
+//class Resource
+//{
+//public:
+//	int m_data[100];
+//
+//public:
+//	Resource()
+//	{
+//		std::cout << "Resource constructed" << std::endl;
+//	}
+//
+//	~Resource()
+//	{
+//		std::cout << "Resource destroyed" << std::endl;
+//	}
+//};
+
+// 15-02
 class Resource
 {
 public:
-	int m_data[100];
+	int* m_data = nullptr;
+	unsigned int m_length = 0;
 
 public:
 	Resource()
 	{
-		std::cout << "Resource constructed" << std::endl;
+		std::cout << "Resource default constructed" << std::endl;
+	}
+
+	Resource(unsigned int length)
+	{
+		std::cout << "Resource length constructed" << std::endl;
+
+		this->m_data = new int[length];
+		this->m_length = length;
+	}
+
+	Resource(const Resource& res)
+	{
+		std::cout << "Resource copy constructed" << std::endl;
+
+		Resource(res.m_length);
+
+		for (unsigned int i = 0; i < m_length; ++i)
+		{
+			m_data[i] = res.m_data[i];
+		}
 	}
 
 	~Resource()
 	{
 		std::cout << "Resource destroyed" << std::endl;
+
+		if (m_data != nullptr)
+		{
+			delete[] m_data;
+		}
+	}
+
+	Resource& operator=(Resource& res)
+	{
+		std::cout << "Resource copy assignment" << std::endl;
+
+		if (&res == this)
+		{
+			return *this;
+		}
+
+		if (this->m_data != nullptr)
+		{
+			delete[] m_data;
+		}
+		
+		m_length = res.m_length;
+		m_data = new int[m_length];
+
+		for (unsigned int i = 0; i < m_length; ++i)
+		{
+			m_data[i] = res.m_data[i];
+		}
+
+		return *this;
+	}
+
+	void print()
+	{
+		for (unsigned int i = 0; i < m_length; ++i)
+		{
+			std::cout << m_data[i] << " ";
+		}
+		std::cout << std::endl;
 	}
 };
