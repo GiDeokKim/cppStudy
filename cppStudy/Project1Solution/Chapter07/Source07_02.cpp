bee8fa1bbe6d045c8d14e3698c8724a2c74ca847
김기덕 <ikid4127@naver.com>
2022-12-11 23:52:23 +0900

    [학습] 7.2 값에 의한 전달

---
+++ cppStudy/Project1Solution/Chapter07/Source07_02.cpp
@@ -0,0 +1,21 @@
+// Passing Arguments by Value(Call by Value)
+#include <iostream>
+
+void doSomething(int y)
+{
+	std::cout << "In func " << y << " " << &y << std::endl;
+}
+
+int main()
+{
+	doSomething(5);
+
+	int x = 6;
+
+	std::cout << "In main " << x << " " << &x << std::endl;
+
+	doSomething(x);
+	doSomething(x + 1);
+
+	return 0;
+}
